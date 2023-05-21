aff406545a1f247bd48b54057f2467662734aa36
Niko Storni <niko@storni.info>
2023-05-21 18:43:06 +0200

    fix data sorting
    fix catch-up

--- main.py
+++ main.py
@@ -25,6 +25,16 @@ def average(lst):
     return sum(lst) / len(lst)
 
 
+def is_too_different(new_value):
+    if len(previousPoints) >= 5:
+        threshold = 0.2  # 20% difference threshold
+        previous_values = previousPoints[:5]  # Get the last five values
+        max_difference = max(previous_values) * threshold  # Calculate the maximum allowed difference
+        if abs(new_value - previous_values[-1]) > max_difference:
+            return True
+    return False
+
+
 # Initialize a list for storing data
 data = []
 
@@ -38,9 +48,10 @@ if os.path.isfile(f"{params['collectionSymbol']}.json"):
     latest_date = datetime.fromisoformat(max(item['date'] for item in data))
 else:
     # If no file exists, set latest_date to now
-    latest_date = datetime.now(timezone.utc)
+    latest_date = datetime(1, 1, 1)
 
 breaker = False
+previousPoints = []
 # Fetch new entries from the API
 while not breaker:
     # Get the next page of results
@@ -59,16 +70,18 @@ while not breaker:
         date = datetime.strptime(date_string, '%a, %d %b %Y %H:%M:%S %Z')
 
         # If this date is older than our most recent date, stop fetching
-        if date.replace(tzinfo=timezone.utc) >= latest_date.replace(tzinfo=timezone.utc):
+        if date.replace(tzinfo=timezone.utc) <= latest_date.replace(tzinfo=timezone.utc):
             breaker = True
             break
 
         new_price = item['listedPrice'] / 100000000.0
 
         # Only add the price if it's not significantly different from the last 5 prices
-        if len(data) < 5 or abs(new_price - average([point['price'] for point in data[-5:]])) / average(
-                [point['price'] for point in data[-5:]]) < 0.2:  # 0.2 is the tolerance, adjust as needed
+        if not is_too_different(new_price):  # 0.2 is the tolerance, adjust as needed
             data.append({'date': date.isoformat(), 'price': new_price})
+        else:
+            print(f"Value too different {date} - {new_price}")
+        previousPoints.insert(0, new_price)
 
     # Print progress
     print(f"Fetched up to offset {params['offset']}...")
@@ -78,6 +91,8 @@ while not breaker:
 
     # Increment offset for the next page
     params['offset'] += 40
+
+data = sorted(data, key=lambda x: datetime.fromisoformat(x["date"]))
 
 # Save data to file
 with open(f"{params['collectionSymbol']}.json", 'w') as file:
